e20f726ad37a061291666b6853d5aae26544105e
qatrainingjaguila <71394922+qatrainingjaguila@users.noreply.github.com>
2020-10-23 13:53:25 +0100

    Update meme.sh

--- meme.sh
+++ meme.sh
@@ -11,4 +11,16 @@ sleep 2
 echo "Never gonna say goodbye"
 sleep 2
 echo "Never gonna tell a lie and hurt you"
+sleep 4
+echo "Never gonna give you up"
 sleep 2
+echo "Never gonna let you down"
+sleep 2
+echo "Never gonna run around and desert you"
+sleep 4
+echo "Never gonna make you cry"
+sleep 2
+echo "Never gonna say goodbye"
+sleep 2
+echo "Never gonna tell a lie and hurt you"
+sleep 4
