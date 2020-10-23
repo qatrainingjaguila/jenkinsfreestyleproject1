94dfaee7c54c1674d90ef49456bc22dc8519c464
qatrainingjaguila <71394922+qatrainingjaguila@users.noreply.github.com>
2020-10-23 13:30:38 +0100

    Create meme.sh

---
+++ meme.sh
@@ -0,0 +1,14 @@
+#!  /bin/bash
+
+echo "Never gonna give you up"
+sleep 2
+echo "Never gonna let you down"
+sleep 2
+echo "Never gonna run around and desert you"
+sleep 4
+echo "Never gonna make you cry"
+sleep 2
+echo "Never gonna say goodbye"
+sleep 2
+echo "Never gonna tell a lie and hurt you"
+sleep 2
